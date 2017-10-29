86bbb36192077d98fc7791f36a50e6765fcd1b61
manisha123 <manishadareddy@gmail.com>
2017-10-29 18:31:02 +0000

    Added validations for email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,11 @@ class User < ActiveRecord::Base
     
     validates :firstname, presence: true
     validates :lastname, presence: true
-    validates :email, presence: true
+    
+    validates :name, presence: true, length: { maximum: 50 }
+    validates :email, presence: true, length: { maximum: 255 }
+    
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, uniqueness: { case_sensetive: false }, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+
 end
