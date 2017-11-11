c35faf0fa26f09db02b26f005d947a7e70cf8ee3
Osama Ali <oali@qatar.cmu.edu>
2017-11-11 15:35:16 +0300

    Fixed authentication problems with design

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106082200) do
+ActiveRecord::Schema.define(version: 20171111120442) do
 
   create_table "branch_products", force: :cascade do |t|
     t.integer "branch_id"
@@ -97,10 +97,10 @@ ActiveRecord::Schema.define(version: 20171106082200) do
     t.string "firstname"
     t.string "lastname"
     t.string "email"
-    t.string "password"
     t.text "dietary_reqs"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
