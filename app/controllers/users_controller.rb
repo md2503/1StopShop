e1bb8fa4209d44ad7ccbb86ab70ba7bef6d004ed
Karen <karencool8@hotmail.com>
2017-11-21 14:13:29 +0000

    trial4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :admin_only, only[:edit, :update, :destroy]
+  before_action :admin_only, only: [:edit, :update, :destroy]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   skip_before_action :authenticate, only: [:new, :create]
 
